0bfb3d7559db456a10ede4a650a3f0a63966ba9a
arafat717 <arafatjibon33@gmail.com>
2023-05-04 12:14:37 +0600

    last commit

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const port = 5000
 
 const cors = require('cors')
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://food-recipe-clint-site.web.app"],
+    methods: "GET",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -12,6 +32,8 @@ app.get('/', (req, res) => {
 const allfoods = require("./data.json")
 
 app.get('/alldata', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','https://food-recipe-clint-site.web.app')
+    res.setHeader('no-cors',true)
     res.send(allfoods)
   })
 app.get('/alldata/:id', (req, res) => {
